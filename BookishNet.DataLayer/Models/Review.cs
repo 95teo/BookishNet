cf86546c9249b64b35db9e526b8ad499ceae2493
95teo <ursan.teofil@yahoo.com>
2017-04-09 19:43:29 +0300

    Solved .gitignore files.

---
+++ BookishNet.DataLayer/Models/Review.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookishNet.DataLayer.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+
+        [ForeignKey("BookId")]
+        public int BookId { get; set; }
+
+        public Book Book { get; set; }
+
+        [Required]
+        public DateTime Timestamp { get; set; }
+
+        public string ReviewText { get; set; }
+
+        public double Stars { get; set; }
+    }
+}
