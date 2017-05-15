f8d9f09273f49344cc73f8f8ae2bbce41d9deb52
95teo <ursan.teofil@yahoo.com>
2017-05-15 22:03:31 +0300

    Added directive and view for sending email.

---
+++ BookishNet.Mvc/wwwroot/app/directives/contactUserDirective.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("BookishNet")
+        .directive("contactUserDirective", contactUserDirective);
+
+    contactUserDirective.$inject = ["$window"];
+
+    function contactUserDirective($window) {
+        // Usage:
+        //     <contactUserDirective></contactUserDirective>
+        // Creates:
+        // 
+        var directive = {
+            restrict: "EA",
+            templateUrl: "/views/UserContact.html"
+        };
+        return directive;
+    }
+
+})();
